8158866c2f01b5c9d08c24b3f2351ba3fcc4f861
yohannmarchand <marchandyohannnnn@gmail.com>
2023-05-03 09:24:46 +0200

    add mes projets section

--- pages/index.vue
+++ pages/index.vue
@@ -12,11 +12,15 @@
 
     <LinePath class="mt-8" :count="8" />
 
-    <SectionTitle title="Je sais pas encore comment le nomer"/>
+    <SectionTitle title="Mon parcours"/>
 
     <section class="px-4 pt-10 max-w-screen-xl md:px-10 mx-auto">
       <HistoryLine :startYear="2018"/>
     </section>
+
+    <LinePath class="mt-8" :count="8" position="right" />
+
+    <SectionTitle title="Mes projets"/>
   </div>
 </template>
 
